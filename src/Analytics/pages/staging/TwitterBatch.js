78e8b08ee03759c04880d341a6b7462503541328
Celine Yuwono <celineyuwono@gmail.com>
2020-04-17 09:55:16 +0900

    Add folder pages.

---
+++ src/Analytics/pages/staging/TwitterBatch.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Widget, WidgetHeader, WidgetContent, ScrollArea } from '@duik/it'
+
+import { Chart, WidgetChartSummary } from '@components'
+import { SelectYear, SelectMonth } from '@composed'
+import { numberFormatZeros } from '@lib'
+
+// import { barData, lineBlank } from './data/chartData'
+import cls from '../analytics-home.module.scss'
+
+import MuiTable from '../../components/MuiTable'
+
+const AnalyticsHome = () => (
+  <ScrollArea className={cls['analytics-home']}>
+    <div className={cls['analytics-home-traffic']}>
+      <MuiTable />
+    </div>
+  </ScrollArea>
+)
+
+export default AnalyticsHome
